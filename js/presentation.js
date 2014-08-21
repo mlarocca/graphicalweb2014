af10e57ce6775846e7e87d35449424aba578b268
mlarocca <marcellolarocca@gmail.com>
2014-08-21 16:44:10 +0200

    init

---
+++ js/presentation.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage' , '6thPage', '7thPage'],
+    sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C', '#C63D0F', '#1BBC9B', '#7E8F7C', '#C63D0F', '#1BBC9B', '#7E8F7C', '#C63D0F', '#1BBC9B', '#7E8F7C'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['Intro', 'Index', 'Data Visualization in Summary', 'Overview', 'Declarative Frameworks', 'Libraries'],
+    slidesNavigation: true
+  });
+
+});
