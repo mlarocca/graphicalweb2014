8920c9d5d586e4dec24b684c25975eafb1a8016e
mlarocca <marcellolarocca@gmail.com>
2014-08-24 13:43:09 +0200

    page 9

--- js/presentation.js
+++ js/presentation.js
@@ -1,11 +1,11 @@
 $(function() {
   $('#fullpage').fullpage({
-    anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage' , '6thPage', '7thPage'],
+    anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage' , '6thPage', '7thPage', '8thPage', '9thPage'],
     sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C', '#C63D0F', '#1BBC9B', '#7E8F7C', '#C63D0F', '#1BBC9B', '#7E8F7C', '#C63D0F', '#1BBC9B', '#7E8F7C'],
     navigation: true,
     navigationPosition: 'right',
-    navigationTooltips: ['Intro', 'Index', 'Data Visualization in Summary', 'Overview', 'Declarative Frameworks', 'MVC Visualization Libraries'],
+    navigationTooltips: ['Intro', 'Index', 'Data Visualization in Summary', 'Overview', 'Declarative Frameworks', 'MVC Visualization Libraries', 'A Comparative Example', 'D3 Implementation', 'Ractive Version'],
     slidesNavigation: true
   });
-
+  hljs.initHighlightingOnLoad();
 });
